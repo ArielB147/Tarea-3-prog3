7b228857fabb0f923441a8b39e5afb667bd1e0da
ArielB147 <95312814+ArielB147@users.noreply.github.com>
2022-11-26 18:51:50 -0400

    Se agrego una estructura diferente en el primer archivo
    Esto lo estoy realizando con el fin de que se provoque un conflicto intencional

--- tarea final.py
+++ tarea final.py
@@ -77,3 +77,6 @@ while rep == True:
         print('la opcion que usted a ingresado no se encuantra disponible eliga otra opción')
         input('')
     print('')
+    r = input('¿le gustaria elegir otra opcion del programa? S o M ')
+    if r == 'M' or r == 'n':
+        rep = False
