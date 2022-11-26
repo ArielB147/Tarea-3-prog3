fbe39bdda105e04f8ab9f6f405de18ef9c98a15e
ArielB147 <95312814+ArielB147@users.noreply.github.com>
2022-11-26 18:39:26 -0400

    Se agrego la estructura de bienvenida

---
+++ tarea final.py
@@ -0,0 +1,37 @@
+import os
+os.system('cls')
+import funtions
+
+
+
+rep = True
+
+while rep == True:
+    os.system('cls')
+    print("""
+
+        ██████  ██ ███████ ███    ██ ██    ██ ███████ ███    ██ ██ ██████   ██████  
+        ██   ██ ██ ██      ████   ██ ██    ██ ██      ████   ██ ██ ██   ██ ██    ██ 
+        ██████  ██ █████   ██ ██  ██ ██    ██ █████   ██ ██  ██ ██ ██   ██ ██ ██ ██ 
+        ██   ██ ██ ██      ██  ██ ██  ██  ██  ██      ██  ██ ██ ██ ██   ██ ██ ██ ██ 
+        ██████  ██ ███████ ██   ████   ████   ███████ ██   ████ ██ ██████   █ ████  
+
+
+        Base de datos de los estudiantes:
+
+        1.Registra un personaje de anime.
+
+        2.reportes.
+
+        3.configuracion.
+
+        4.Exportar datos de los estudiantes a un archivo HTML.
+
+        5.acerca de.
+
+        6.salir el programa.
+
+        """)
+    
+
+    respuesta = input('ingrese el numero de la accion que desea realizar ')
